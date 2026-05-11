ae8587bd9d8bf9f0309a556c26ae03ade8ddd31c
Veronica Paz Ledo Balke <167806689+verxkkj@users.noreply.github.com>
2026-05-11 13:02:18 -0300

    Create api.turma02.ts

---
+++ src/prog2-att-extra1/src/api.turma02.ts
@@ -0,0 +1,99 @@
+import todo from "./core.ts";
+
+const server = Bun.serve({
+  port: 3000,
+
+  routes: {
+    "/": new Response(Bun.file("./public/index.html")),
+
+    "/api/todo": {
+      GET: async () => {
+        const items = await todo.getItems()
+        return Response.json(items)
+      },
+
+      POST: async (req) => {
+        const data = await req.json() as any;
+        const item = data.item || null;
+        if (!item)
+          return Response.json('Por favor, forneça um item para adicionar.', { status: 400 });
+        await todo.addItem(item);
+        return Response.json(data);
+      },
+    },
+
+    "/api/todo/:index": {
+      PUT: async (req) => {
+        const index = parseInt(req.params.index);
+        if (isNaN(index))
+          return Response.json('Índice inválido. um número inteiro é esperado.', { status: 400 });
+        const data = await req.json() as any;
+        const newItem = data.newItem || null;
+        if (!newItem)
+          return Response.json('Por favor, forneça um novo item para atualizar.', { status: 400 });
+        try {
+          await todo.updateItem(index, newItem);
+          return Response.json(`Item no índice ${index} atualizado para "${newItem}".`);
+        } catch (error: any) {
+          return Response.json(error.message, { status: 400 });
+        }
+      },
+
+      DELETE: async (req) => {
+        const index = parseInt(req.params.index);
+        if (isNaN(index))
+          return Response.json('Índice inválido.', { status: 400 });
+        try {
+          await todo.removeItem(index);
+          return Response.json(`Item no índice ${index} removido com sucesso.`);
+        } catch (error: any) {
+          return Response.json(error.message, { status: 400 });
+        }
+      },
+    },
+
+    // exemplo basico:
+
+    "/api/exemplo": {
+      GET: () => {
+        return new Response(`Esse é o exemplo: ${Date.now()}`)
+      },
+
+      POST: async (req) => {
+        const data = await req.json() as any;
+        data.recebidoEm = new Date().toLocaleDateString("pt-BR");
+        return Response.json(data);
+      },
+    },
+
+    "/api/exemplo/:id": {
+      PUT: async (req, params) => {
+        const { id } = req.params;
+        const data = await req.json() as any;
+        data.id = id;
+        data.recebidoEm = new Date().toLocaleDateString("pt-BR");
+        return Response.json(data);
+      },
+
+      PATCH: async (req, params) => {
+        const { id } = req.params;
+        const data = await req.json() as any;
+        data.chavesAtualizadas = Object.keys(data);
+        data.id = id;
+        data.atualizadoEm = new Date().toLocaleDateString("pt-BR");
+        return Response.json(data);
+      },
+
+      DELETE: (req, params) => {
+        const { id } = req.params;
+        return new Response(`Recurso com id ${id} deletado`, { status: 200 });
+      }
+    }
+  },
+
+  async fetch(req) {
+    return new Response(`Not Found`, { status: 404 });
+  },
+});
+
+console.log(`Server running at http://localhost:${server.port}`);
